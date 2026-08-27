4d082c55084960986d593946ef6dbbe278ccb864
parthsoni05 <parthbro1675@gmail.com>
2026-08-27 19:08:46 +0530

    Explore Chapter_02 commands

---
+++ Chapter_02/create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE teachers (
+    t_id bigserial,
+    first_name varchar(25),
+    last_name varchar(50),
+    school varchar(50),
+    hire_date date,
+    salary numeric
+);
---
+++ Chapter_02/insert_values.sql
@@ -0,0 +1,7 @@
+INSERT INTO teachers (first_name, last_name, school, hire_date, salary)
+VALUES ('Rahul', 'Joshi', 'F.D. Roosevelt HS', '2011-10-30', 36200),
+       ('Jai', 'Khatri', 'F.D. Roosevelt HS', '1993-05-22', 65000),
+       ('Samarth', 'Lodhi', 'Myers Middle School', '2005-08-01', 43500),
+       ('Samaira', 'Bishnoi', 'Myers Middle School', '2011-10-30', 36200),
+       ('Badri', 'Nath', 'Myers Middle School', '2005-08-30', 43500),
+       ('Raziya', 'Qureshi', 'F.D. Roosevelt HS', '2010-10-22', 38500);
